269290f3b4f216aea630f2c3d2a0b0dc61053468
Tom Holder <tdjholder@gmail.com>
2018-09-25 21:33:41 -0400

    Fix typos

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Making the output of DiffLib Bootstrap compatible",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # url="https://github.com/pypa/sampleproject",
+    url="https://github.com/tdjholder/bootstrap-difflib",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
