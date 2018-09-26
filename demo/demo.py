51b2ab97c63c58ff847657d5761279b04f1831b5
Tom Holder <tdjholder@gmail.com>
2018-09-25 21:34:46 -0400

    add a demo

---
+++ demo/demo.py
@@ -0,0 +1,20 @@
+from lib.bootstrap_difflib import BootstrapHtmlDiff
+
+print(__name__)
+if __name__ == "__main__":
+    toLines = ['This project was born out of the idea', 'Of wanting a slightly better', 'Looking Diff Table']
+    fromLines = ['This project has been born out of the idea', 'Of wanting a slightly better', 'GUI']
+
+    import os
+    import webbrowser
+
+    t = BootstrapHtmlDiff("https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css")
+    path = os.path.abspath('temp.html')
+    url = 'file://' + path
+    html = t.make_file(toLines, fromLines)
+    print(html)
+    with open(path, 'w') as f:
+        f.write(html)
+    webbrowser.open(url)
+
+    f.close()
